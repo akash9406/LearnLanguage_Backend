930634f7369da330bce3683e71d772444269648c
akash9406 <90980359+akash9406@users.noreply.github.com>
2024-02-21 21:16:46 +0530

    Update Auth.js

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -14,7 +14,7 @@ const auth = async (req, res, next) => {
       next();
     }
   } catch (error) {
-    res.status(401).json({ error: "Session expired" });
+    res.status(401).json({ error: error });
   }
 };
 
