74b2d4cf7de88562abbbfbaeb87a8f36db78b4a1
akash9406 <90980359+akash9406@users.noreply.github.com>
2024-02-21 21:55:34 +0530

    Update Auth.js

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,21 +1,21 @@
 const jwt = require("jsonwebtoken");
 const model = require("../model/User");
 const User = model.User;
-let val  = {}
 const auth = async (req, res, next) => {
   try {
     const cookie = req.cookies;
-    val = cookie;
+      console.log(`change ${req.cookies}`)
     var decoded = jwt.verify(cookie.token, process.env.JWT_SECRET);
     if (decoded.id) {
       //req.user here is a key which we have create in  req object which will be pass down to next()
+    
       req.user = await User.findById(decoded.id)
         .select("-token")
         .select("-password");
       next();
     }
   } catch (error) {
-    res.status(401).json({ error: val });
+    res.status(401).json({ error: error });
   }
 };
 
